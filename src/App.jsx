bcd9b0e5934d161540b611b8735c880dd1930c9a
AryaShrestha05 <aryashrestha005@gmail.com>
2025-09-14 12:35:18 -0400

    second commit, login pages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './components/Login'
-
+import ClientSignUp from './components/ClientSignUp'
+import AttorneySignUp from './components/AttorneySignUp'
+import AttorneyLogin from './components/AttorneyLogin' 
 
 const App = () => {
   return (
